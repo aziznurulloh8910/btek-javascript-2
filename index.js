67138fbcad5fdd016ec24d05c81c4e7008b00fea
Aziz <aziznurulloh8@gmail.com>
2022-10-15 01:34:56 +0700

    [ADD] While Promise

--- index.js
+++ index.js
@@ -4,53 +4,45 @@ const currency = require('currency-formatter')
 var rl = readline.createInterface(process.stdin, process.stdout)
 
 const hitungOngkir = ()=> {
-  rl.question('Input Jarak (km) : ', (jarak)=> {
-    if(jarak <= 2){
-      var ongkir = 8000
-    } else {
-      ongkir = jarak * 5000
-    }
-    
-    biayaLayanan = ongkir * 0.045
-    hasil = console.log("Total Ongkos Kirim : "+ currency.format(ongkir,{code: 'IDR'})
-      +"\nBiaya Layanan : "+ currency.format(biayaLayanan,{code: 'IDR'}))
-    
-    return hasil
-  })
-}
+  return new Promise((resolve, reject) => {
+    rl.question('\nInput Jarak (km) : ', (jarak)=> {
+      try {
+        jarak = parseInt(jarak)
+        
+        if(!jarak){
+          reject()
+        }
 
-const loop = ()=> {
-  let run = true
-  while(run) {
-    try{
-      hitungOngkir()
-    }catch(error){
-      run = false
-      console.log('exit')
-    }
-  }
-  rl.close()
-}
+        if(jarak <= 2){
+          var ongkir = 8000
+        } else {
+          ongkir = jarak * 5000
+        }
+        
+        biayaLayanan = ongkir * 0.045
+        console.log("Total Ongkos Kirim : "+ currency.format(ongkir,{code: 'IDR'})
+          +"\nBiaya Layanan : "+ currency.format(biayaLayanan,{code: 'IDR'}) + '\nInput Huruf Untuk Keluar')
+        
+        resolve(true)
+      } catch (error) {
 
-const a = (b) => {
-  return new Promise((resolve, reject)=> {
-    try{
-      if (jarak.toLowerCase() === 'q') {
-        throw new Error('quit')
       }
-      resolve(b)
-    } catch (error) {
-      reject(error)
-    }
+    })
   })
 }
 
+const app = async() => {
+  let run = true
 
-const app = async ()=> {
-  try {
-    await a(loop())    
-  } catch(err) { 
-    console.log(err.message)
+  while(run) {
+    try {
+      const data = await hitungOngkir()
+      run = data
+    } catch(error) {
+      console.log('Thanks')
+      run = false
+      rl.close()
+    }
   }
 }
 
